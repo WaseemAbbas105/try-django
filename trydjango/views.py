9dbe183c018013cd5589b33078c93b607eb8729c
waseem abbas <waseem.zafar@axiomworld.net>
2022-03-20 09:59:47 +0000

    [add] new features to articles app

--- trydjango/views.py
+++ trydjango/views.py
@@ -4,14 +4,32 @@ To render html web pages
 
 # from urllib import response
 from django.http import HttpResponse
+from articles.models import Article
+from django.template.loader import render_to_string
+import random
 
 
 
-def home_view(request):
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    random_id = random.randint(1,5)
+    print(random_id)
 
-    name = "waseem"
+    article_obj = Article.objects.get(id=random_id)
+    article_queryset = Article.objects.all()
+    context = {
+        "article_queryset":article_queryset,
+        "title": article_obj.title,
+        "contect": article_obj.contect,
+    }
 
-    HTML_STRING = f"""
-<h2>Hello {name}</h2>
-"""
+    HTML_STRING = render_to_string("home-view.html", context=context) 
+    
+    
+#     """
+# <h1>{title}</h1>
+# <p>{contect}<p>
+# """.format(**context)
+    # HTML_STRING = H_STRING + P_STRING
+
     return HttpResponse(HTML_STRING)
